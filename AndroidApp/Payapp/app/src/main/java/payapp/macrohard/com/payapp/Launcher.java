75b0f3d37475d4f8afcdfc45e91cdfad4e458cde
Nishant Bisen <nishantb@opera.com>
2015-08-18 01:46:32 -0700

    Add Material Design to the App (values-v21/style.xml)

--- AndroidApp/Payapp/app/src/main/java/payapp/macrohard/com/payapp/Launcher.java
+++ AndroidApp/Payapp/app/src/main/java/payapp/macrohard/com/payapp/Launcher.java
@@ -1,9 +1,11 @@
 package payapp.macrohard.com.payapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+
+/*
+We are using AppCompat library to support older version of Android
+ */
 
 public class Launcher extends AppCompatActivity {
 
@@ -11,27 +13,5 @@ public class Launcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_launcher, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
